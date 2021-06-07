b3b4567ff7e1303852e04a1c5645ddcd94e016c2
purpleeeee <h.purpleeeee@gmail.com>
2021-06-08 00:15:32 +0900

    chore: rtk

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider resetCSS={true}>
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider resetCSS={true}>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
